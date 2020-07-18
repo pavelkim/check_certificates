ba67908cc59a373d399a17bb50dce10beeecc47d
Pavel Kim <hello@pavelkim.ru>
2020-07-18 19:52:11 +0300

    [07-2020][Add] Initial code base state. Readme.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # check_certificates
-Monitor your HTTPS SSL Certificate's expiration.
+
+Use this script to automate HTTPS SSL Certificate monitoring. It curl's remote server on 443 port and then verifies remote SSL Certificate's expiration date.
+
+# Usage
+
+The script takes on input a file with a list of hostnames:
+```bash
+./check_certificate input_filename.txt
+```
+
+# Input file format
+
+Example of an input file contents:
+```
+example.com
+google.com
+mail.com
+imaginary-domain-9000.com
+```
+
+# Output data format
+
+Output fields: 
+```
+Hostname    Valid Not Before    Valid Not After    Expired in N Days
+```
+
+Example outpupt:
+```
+imaginary-domain-9000.com  error                error                -1
+google.com                 2020-06-30 20:43:12  2020-09-22 20:43:12  66
+example.com                2018-11-28 00:00:00  2020-12-02 12:00:00  136
+mail.com                   2018-01-15 00:00:00  2021-01-14 12:00:00  179
+```
---
+++ check_certificates.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#
+# Checks if SSL certificate on https server is valid.
+#
+# Usage: 
+#  ./check_certificate input_filename.txt
+#
+# Input file format:
+#  domain1.com
+#  domain2.com
+#  domain3.com
+# 
+# Result output format:
+#  domain1.com  error                error                -1
+#  domain2.com  2013-10-31 00:00:00  2016-10-30 23:59:59  92
+#  domain2.com  2015-12-03 00:00:00  2016-12-02 23:59:59  125
+#
+#
+# v1.0 2017-09-23 Basic checker (Pavel Kim)
+# v1.1 2018-05-07 Better logging (Pavel Kim)
+# v1.2 2020-07-18 Better result output (Pavel Kim)
+
+set -o pipefail
+
+[[ "${DEBUG}" == "1" ]] && GLOBAL_LOGLEVEL="7" || GLOBAL_LOGLEVEL="0"
+[[ ! -z "$1" ]] && INPUT_FILENAME="$1" || { echo "Set the input file as the 1st parameter"; exit 2; }
+
+[[ -f "${INPUT_FILENAME}" ]] || { echo "Can't open input file: ${INPUT_FILENAME}"; exit 2; }
+
+timestamp() {
+    date "+%F %T"
+}
+
+info() {
+
+	local msg="$1"
+	local self_level=3
+	local self_level_name="info"
+
+	if [[ "${self_level}" -le "${GLOBAL_LOGLEVEL}" ]]
+	then	
+		ts="$(timestamp)"
+		echo "[${ts}] [${self_level_name}] [${FUNCNAME[1]}] $msg" >&2
+		return 0
+	fi
+}
+
+warning() {
+
+	local msg="$1"
+	local self_level=2
+	local self_level_name="warning"
+
+	if [[ "${self_level}" -le "${GLOBAL_LOGLEVEL}" ]]
+	then	
+		ts="$(timestamp)"
+		echo "[${ts}] [${self_level_name}] [${FUNCNAME[1]}] $msg" >&2
+		return 0
+	fi
+}
+
+
+check_https_certificate_dates() {
+
+	[[ ! -z "$1" ]] && local remote_hostname="$1" || { warning "Remote hostname not set!"; return 100; }
+	
+	local result
+	local final_result
+	local dates=( )
+	
+	info "Starting https ssl certificate validation for ${remote_hostname}"
+	result="$( echo | openssl s_client -servername "${remote_hostname}" -connect "${remote_hostname}:443" 2>/dev/null | openssl x509 -noout -dates | cut -d"=" -f2  )"
+	RC=$?
+	
+	if [[ "${RC}" != "0" ]]; then
+		warning "Can't process openssl output for ${remote_hostname}"
+		final_result="${remote_hostname} error error"
+	else
+		while read line; do
+			dates+=( "${line}" )
+		done <<< "${result}"
+	
+		info "${remote_hostname} Not before ${dates[0]}"
+		info "${remote_hostname} Not after ${dates[1]}"
+		
+		final_result="${remote_hostname} $(date -d "${dates[0]}" "+%s") $(date -d "${dates[1]}" "+%s")"
+	fi
+	
+	echo "${final_result}"
+	return "${RC}"
+}
+
+full_result=( )
+error_result=( )
+formatted_result=( )
+today_timestamp="$(date "+%s")"
+sorted_result=( )
+
+while read -r remote_hostname; do 
+	info "Processing '${remote_hostname}'"
+	current_result=$( check_https_certificate_dates "${remote_hostname}" )
+	rc="$?"
+	
+	if [[ "${rc}" != "0" ]]; then
+		warning "Skipping '${remote_hostname}'"
+		error_result+=( "${remote_hostname}" )
+	else
+		full_result+=( "${current_result}" )
+	fi
+	info "Finished processing '${remote_hostname}'"
+done < "${INPUT_FILENAME}"
+
+if [[ "${#full_result[@]}" -le "0" ]]; then
+	warning "Couldn't process anything from ${INPUT_FILENAME}"
+fi
+
+if [[ "${#error_result[@]}" -gt "0" ]]; then
+	for error_item in "${error_result[@]}"; do
+		formatted_result+=( "${error_item}	error	error	-1" )
+	done
+fi
+
+for result_item in "${full_result[@]}"; do
+	result_item_parts=( ${result_item} )
+	formatted_result+=( "${result_item_parts[0]}	$(date -d "@${result_item_parts[1]}" "+%F %T")	$(date -d "@${result_item_parts[2]}" "+%F %T")	$(( (result_item_parts[2] - today_timestamp) / 86400 ))" )
+done
+
+while read formatted_item; do
+	sorted_result+=( "${formatted_item}" )
+done <<< "$( IFS=$'\n' ; echo "${formatted_result[*]}" | sort -n -k6)"
+
+(IFS=$'\n'; echo "${sorted_result[*]}" | column -t -s$'\t')
